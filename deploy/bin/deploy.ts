c8fb14ea3d1be039f0571b3e6b601b1cf9d97ea3
dev-ashishpal <ashish79276@gmail.com>
2025-06-19 17:39:46 +0530

    Add AWS CDK

---
+++ deploy/bin/deploy.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { DeployStack } from "../lib/deploy-stack";
+
+const app = new cdk.App();
+new DeployStack(app, "DeployStack", {});
+
+app.synth();
---
+++ deploy/lib/deploy-stack.ts
@@ -0,0 +1,25 @@
+import * as cdk from "aws-cdk-lib";
+import {
+    CodePipeline,
+    CodePipelineSource,
+    ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DeployStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        new CodePipeline(this, "CodePipeline", {
+            pipelineName: "MyPipeline",
+            synth: new ShellStep("Synth", {
+                input: CodePipelineSource.gitHub(
+                    "dev-ashishpal/QUIZ-APP-playpower-",
+                    "main"
+                ),
+                commands: ["npm ci", "npm run build", "npx cdk synth"],
+            }),
+        });
+    }
+}
